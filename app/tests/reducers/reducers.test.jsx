9c915f2dfe0d69bfc15fec91406a0ac8ff47e469
Paul McClure <paulage@gmail.com>
2017-01-05 12:52:08 -0800

    add reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,67 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+    describe('searchTextReducer', () => {
+        it('should set searchText', () => {
+            var action = {
+                type: 'SET_SEARCH_TEXT',
+                searchText: 'dog'
+            }
+
+            //pass in empty string (current state) and action above
+            //df deep freeze is used to make sure that nothing is altered. If the empty string and action are updated at all the test will fail
+            var res = reducers.searchTextReducer(df(''), df(action));
+
+            expect(res).toEqual(action.searchText);
+        })
+    });
+
+    describe('showCompletedReducer', () => {
+        it('should toggle show completed', () => {
+            var action = {
+                type: 'TOGGLE_SHOW_COMPLETED',                
+            }
+
+            //pass in empty string (current state) and action above
+            var res = reducers.showCompletedReducer(df(false), df(action));
+
+            expect(res).toEqual(true);
+        })
+    });
+
+    describe('todosReducer', () => {
+        it('should add a todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'walk the dog'  
+            }
+            
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        })
+
+           it('should toggle todo', () => {
+            var todos = [{
+                id: 123,
+                text: 'Something',
+                completed: true,
+                createdAt: 123,
+                completedAt: 125
+            }];
+            
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 123
+            }
+            var res = reducers.todosReducer(df(todos), df(action));
+
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
+        })
+    });
+});
